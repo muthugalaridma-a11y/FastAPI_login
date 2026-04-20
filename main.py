0800b4b9d703491c6214f6c4b9874799c0a6e38a
muthugalaridma-a11y <muthugala.ridma@iisgalvanimi.edu.it>
2026-04-20 10:05:53 +0000

    scompaiono

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from fastapi import fastAPI
-from fastaspi.responses import FileResponse
-from fastapi.staticfiles import staticfiles
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 app = FastAPI()
-app.mount("static", StaticFiles(directory="static"),name="static")#per montare i dati dentro l'app
+app.mount("/static", StaticFiles(directory="static"),name="static")#per montare i dati dentro l'app
 @app.get("/") #  Endpoint,punto in cui andiamo a chiamare il server web
 def home():
     # Restituisce direttamente il file HTML
-    return FileResponse('static/home.html')
-    
+    return FileResponse('static/index.html')
+
+@app.get("/login")
+def controlla(username: str,password:str):
+    print ("username",username,"password", password)
+    if username == "admin" and password == "x":
+        risposta={"messaggio":1}
+    else:
+        risposta ={"messaggio": 0}
+    return (risposta)
--- static/script.js
+++ static/script.js
@@ -3,8 +3,21 @@ async function controllaCredenziali() {
     const password = document.getElementById('password').value;
     if (!username || !password) 
         return alert("Scrivi uno username e password!");
-    const res = await fetch(`/login?username=${username}&password=${"password"}`);
+    const res = await fetch(`/login?username=${username}&password=${password}`);
     const dati = await res.json();
-    document.getElementById('risultato').innerText = dati.messaggio;
+    //document.getElementById('risultato').innerText = dati.messaggio;
+    if (dati.messaggio==1){
+        document.getElementById('risultato').innerText="benevenuto nel nostro sito";
+        document.getElementById("risultato").style.fontSize = "50px";
+        document.getElementById("risultato").style.marginTop = "0px";
+        document.getElementById("username").remove();
+        document.getElementById("password").remove();sjsjns'
+        
+        document.getElementById("btn_registrati").remove();
+        
+    }
+    else
+        document.getElementById('risultato').innerText="riprova";
 }
-document.getElementById('btn_registatrati').addEventListener('click', controllaCredenziali);
+
+document.getElementById('btn_registrati').addEventListener('click', controllaCredenziali);
